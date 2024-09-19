858e9487bd45b1fa54a6a10a65d2295c087fc496
Subham Kumar <53145627+subham0kumar@users.noreply.github.com>
2024-09-19 19:52:24 +0530

    Card Created

---
+++ src/Components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({
+  ingredients,
+  categories,
+  nutrition_grade,
+  img_url,
+  product_name,
+  id,
+}) => {
+  return (
+    <div className="card">
+      <div className="card-image"></div>
+      <div className="category"> {nutrition_grade} </div>
+      <div className="heading">
+        {product_name}
+        <div className="author">
+          {" "}
+          By <span className="name">Abi</span> 4 days ago
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
